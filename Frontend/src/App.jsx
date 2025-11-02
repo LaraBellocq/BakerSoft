eaaf7162ed2c25619564cdc18355298d838aab5d
mllmicaelalopez <micaelalucialopez23@gmail.com>
2025-11-02 02:37:43 +0100

    Revert "Actualizacion rutas-import"
    This reverts commit b12540d4ef2fceb01ca9f16d314a7ee475820855.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
-import Dashboard from './Dashboard/pages/Dashboard.jsx';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Login from './features/auth/Login.jsx';
+import { Dashboard } from './Dashboard';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<Navigate to="/login" replace />} />
+    </Routes>
   );
 }
 
